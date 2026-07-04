70b8411452aae575370c61ed93c1d2b2c1a7d852
Kareem Tamer Yousef <kareemtamer2512@gmail.com>
2026-07-04 08:26:45 +0300

    fix: config.py missing import os

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
 ABUSEIPDB_API_KEY = os.environ.get("ABUSEIPDB_API_KEY", "")
-
 VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL_API_KEY", "")
+OLLAMA_URL = os.environ.get("OLLAMA_URL", "http://localhost:11434/api/generate")
+OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL", "llama3.2")
